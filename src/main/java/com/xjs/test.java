db262aa278570db6ac3a77ff04327b0bbeb4ed39
ChesterXie <xjs675360892@163.com>
2020-06-30 22:40:34 +0800

    second commit

--- src/main/java/com/xjs/test.java
+++ src/main/java/com/xjs/test.java
@@ -3,5 +3,7 @@ package com.xjs;
 public class test {
     public static void main(String[] args) {
         System.out.println("helloword");
+        System.out.println("helloword1111111");
+
     }
 }
